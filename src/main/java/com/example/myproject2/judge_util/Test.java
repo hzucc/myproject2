ba810d929f3d6d73f56557d1068a90dc323f8ca5
cc <122130826@qq.com>
2019-11-07 03:02:28 +0800

    2019-11-07 02:53 提高评测可靠性（事务管理） 将所有Mybatis的Map返回值改为类 二次改进Docker的生产消费方式

--- src/main/java/com/example/myproject2/judge_util/Test.java
+++ src/main/java/com/example/myproject2/judge_util/Test.java
@@ -11,25 +11,6 @@ import java.io.File;
  * */
 public class Test {
     public static void main(String[] args) throws Exception {
-        String codeType = "c/c++";
-        JudgeCode judgeCode = new JudgeCode();
-        CompileParam compileParam = new CompileParam();
-        compileParam.setCompileFile(new File("/myproject2/compile/t2/Main.cpp"));
-        compileParam.setCodeType(codeType);
-        CompileResult compileResult = judgeCode.compile(compileParam);
 
-        if (compileResult.isCompileSuccess()) {
-            RunParam runParam = new RunParam();
-            runParam.setInput(new File("/myproject2/testdata/problemId_1041/t1/test.in"));
-            runParam.setOutput(new File("/myproject2/testdata/problemId_1041/t1/test.out"));
-            runParam.setRunFile(compileResult.getRunFile());
-            runParam.setCodeType(codeType);
-            runParam.setTimeLimit(2);
-            runParam.setMemoryLimit(400);
-            RunResult runResult = judgeCode.run(runParam);
-            System.out.println(runResult);
-        } else {
-            System.out.println(compileResult.getErrorMessage());
-        }
     }
 }
